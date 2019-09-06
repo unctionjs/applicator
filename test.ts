4c10f0e961726a6fe00e6ac10aa616d58c32c27a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 13:08:06 -0700

    Continuing to setup testing

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-magic-numbers */
-import applicator from "./index.ts";
+import applicator from "./index";
 
 test(() => {
   expect(applicator((value) => value + 1)(1)).toEqual(2);
